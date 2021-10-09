44fae24876bbc9305d05e1273f6120ece9ee539c
Finn-Thorben Sell <dev@finn-thorben.me>
2021-10-09 22:33:54 +0200

    display tun device IP addresses in normal logging

--- src/tun_management.rs
+++ src/tun_management.rs
@@ -1,5 +1,5 @@
 use crate::ip_addrs;
-use log::debug;
+use log::info;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use tokio_tun::{Tun, TunBuilder};
 
@@ -14,26 +14,27 @@ pub async fn create_tun_devices(
         match network {
             IpAddr::V4(network) => {
                 let netmask_size = ip_addrs::calc_netmask_size_with_n_hosts4(n_hosts);
+                let tun_address = ip_addrs::get_nth_address_in_network4(1, netmask_size, &network);
                 let tun = create_ipv4_tun_device(
                     &format!("{}{}", base_name, i),
-                    ip_addrs::get_nth_address_in_network4(1, netmask_size, &network),
+                    tun_address,
                     ip_addrs::calc_netmask_from_size4(netmask_size),
                 )
                 .await;
+                info!("Created TUN device [own_address={}]", tun_address);
                 result.push(tun);
             }
             IpAddr::V6(network) => {
                 let netmask_size = ip_addrs::calc_netmask_size_with_n_hosts6(n_hosts);
+                let tun_address =
+                    ip_addrs::get_nth_address_in_network6(1, netmask_size as usize, &network);
                 let tun = create_ipv6_tun_device(
                     &format!("{}{}", base_name, i),
-                    crate::ip_addrs::get_nth_address_in_network6(
-                        1,
-                        netmask_size as usize,
-                        &network,
-                    ),
+                    tun_address,
                     netmask_size as u32,
                 )
                 .await;
+                info!("Created TUN device [own_address={}]", tun_address);
                 result.push(tun);
             }
         }
@@ -47,7 +48,7 @@ async fn create_ipv4_tun_device(
     device_address: Ipv4Addr,
     netmask: Ipv4Addr,
 ) -> Tun {
-    match TunBuilder::new()
+    TunBuilder::new()
         .name(device_name)
         .tap(false) // tap would be ethernet bridging but we are only interested in IP packets
         .address(IpAddr::V4(device_address))
@@ -55,16 +56,7 @@ async fn create_ipv4_tun_device(
         .packet_info(false)
         .up() // automatically bring the device online (instead of having to run `ip link set <name> up`)
         .try_build()
-    {
-        Ok(tun) => {
-            debug!(
-                "Created tun device [name={}, address={}]",
-                device_name, device_address
-            );
-            tun
-        }
-        Err(err) => panic!("Could not create TUN device [error={}]", err),
-    }
+        .expect("Could not create TUN device")
 }
 
 async fn create_ipv6_tun_device(
@@ -72,7 +64,7 @@ async fn create_ipv6_tun_device(
     device_address: Ipv6Addr,
     prefix_length: u32,
 ) -> Tun {
-    match TunBuilder::new()
+    TunBuilder::new()
         .name(device_name)
         .tap(false)
         .address(IpAddr::V6(device_address))
@@ -80,14 +72,5 @@ async fn create_ipv6_tun_device(
         .packet_info(false)
         .up()
         .try_build()
-    {
-        Ok(tun) => {
-            debug!(
-                "Created tun device [name={}, address={}]",
-                device_name, device_address
-            );
-            tun
-        }
-        Err(err) => panic!("Could not create TUN device [error={}]", err),
-    }
+        .expect("Could not create TUN device")
 }
